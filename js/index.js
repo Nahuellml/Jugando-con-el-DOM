e7e6bdc96b1a280e6e3c8d2578d02a13559a688f
Nahuel Alvarez <alvareznahue@gmail.com>
2024-01-17 20:17:08 -0300

    detecciones

--- js/index.js
+++ js/index.js
@@ -6,6 +6,9 @@ import scrollTopButton from "./dom/boton_scroll.js";
 import darkTheme from "./dom/tema_oscuro.js";
 import responsiveMedia from "./dom/objeto_responsive.js";
 import responsiveTester from "./dom/prueba-responsiva.js";
+import userDeviceInfo from "./dom/deteccion_dispositivos.js";
+import networkStatus from "./dom/deteccion_red.js";
+import webCam from "./dom/deteccion_webcam.js";
 
 const d = document;
 
@@ -21,6 +24,8 @@ d.addEventListener('DOMContentLoaded', (e) => {
     responsiveMedia('youtube', '(min-width: 1024px)', `<a href="https://youtu.be/V-1SlwUAmwY?si=AovQzgvxZ-nSPAGs" target="_blank" rel="noopener">Ver Video</a>`,`<iframe width="560" height="315" src="https://www.youtube.com/embed/V-1SlwUAmwY?si=MuHlRm_meRX_wEL5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`);
     responsiveMedia('gmaps', '(min-width: 1024px)', `<a href="https://maps.app.goo.gl/ekB1VpqinA5tfQnZA" target="_blank" rel="noopener">Ver Mapa</a>`, `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3348.12484339357!2d-60.63304072356931!3d-32.947713173592845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b7ab1dc9b6d36f%3A0x97b9e9584d94d851!2sMonumento%20Hist%C3%B3rico%20Nacional%20a%20la%20Bandera!5e0!3m2!1ses!2sar!4v1705336647299!5m2!1ses!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`);
     responsiveTester('responsive-tester');
+    userDeviceInfo('user-device');
+    webCam('webcam');
   });
   
   d.addEventListener('keydown', e => {
@@ -29,3 +34,4 @@ d.addEventListener('DOMContentLoaded', (e) => {
   });
   
   darkTheme('.dark-theme-btn', 'dark-mode');
+  networkStatus();
