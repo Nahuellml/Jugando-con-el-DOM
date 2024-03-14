de1a70ae2998422d4fd25d239a9a820b70750dd6
Rocio <rolinoe@gmail.com>
2024-03-14 14:02:21 -0300

    narrador 2

--- js/index.js
+++ js/index.js
@@ -16,6 +16,7 @@ import slider from "./dom/carrusel.js";
 import scrollSpy from "./dom/scroll_espia.js";
 import smartVideo from "./dom/video_inteligente.js";
 import contactFormValidations from "./dom/validaciones_formulario.js";
+import speechReader from "./dom/narrador.js";
 
 const d = document;
 
@@ -49,3 +50,4 @@ d.addEventListener('DOMContentLoaded', (e) => {
   
   darkTheme('.dark-theme-btn', 'dark-mode');
   networkStatus();
+  speechReader();
